0c6534158c30f0d358eb53e65449206e9b471a1a
Uzair-khane <uzairkhanar123@gmail.com>
2026-05-06 15:01:36 +0500

    bookin syestem complete

--- components/Modal/booking.vue
+++ components/Modal/booking.vue
@@ -1,70 +1,65 @@
 <template>
   <div class="text-black">
-    <IconClose
-      @click="emit('toggleModal')"
-      class="w-5 cursor-pointer h-5 absolute right-2 top-1"
-    />
+    <IconClose @click="emit('toggleModal')" class="w-5 cursor-pointer h-5 absolute right-2 top-1" />
 
     <h3 class="text-center font-semibold mt-2 text-2xl">Booking Details</h3>
     <div class="mt-3 space-y-4 text-sm">
       <div class="flex gap-2 flex-wrap">
-        <label class="min-w-28 flex items-center gap-2" for=""
-          ><IconFromLocation class="w-4 h-4 fill-black" />
-          <span> From location: </span></label
-        >
+        <label class="min-w-28 flex items-center gap-2" for="">
+          <IconFromLocation class="w-4 h-4 fill-black" />
+          <span> From location: </span>
+        </label>
         <p class="font-semibold">
           {{ booking.from_location }}
         </p>
       </div>
       <div v-if="booking.drop_location" class="flex gap-2 flex-wrap">
-        <label class="min-w-28 flex items-center gap-2" for=""
-          ><IconToLocation class="w-4 h-3 fill-black" />
-          <span> To location: </span></label
-        >
+        <label class="min-w-28 flex items-center gap-2" for="">
+          <IconToLocation class="w-4 h-3 fill-black" />
+          <span> To location: </span>
+        </label>
         <p class="font-semibold">
           {{ booking.drop_location }}
         </p>
       </div>
       <div v-if="booking.distance" class="flex gap-2 flex-wrap">
-        <label class="min-w-28 flex items-center gap-2" for=""
-          ><IconFromLocation class="w-4 h-4 fill-black" />
-          <span> Distance: </span></label
-        >
+        <label class="min-w-28 flex items-center gap-2" for="">
+          <IconFromLocation class="w-4 h-4 fill-black" />
+          <span> Distance: </span>
+        </label>
         <p class="font-semibold">{{ booking.distance }}</p>
       </div>
       <div class="flex gap-2 flex-wrap">
-        <label class="min-w-28 flex items-center gap-2" for=""
-          ><IconCost class="w-4 h-3 fill-black" />
-          <span> Total cost: </span></label
-        >
-        <p class="font-semibold">{{ booking.cost }} AED</p>
+        <!-- <label class="min-w-28 flex items-center gap-2" for="">
+          <IconCost class="w-4 h-3 fill-black" />
+          <span> Total cost: </span>
+        </label> -->
+        <!-- <p class="font-semibold">PKR {{ booking.cost }}</p> -->
       </div>
       <div class="flex gap-2 flex-wrap">
-        <label class="min-w-28 flex items-center gap-2" for=""
-          ><IconCalender class="w-4 h-4 stroke-black" />
-          <span> Pickup date: </span></label
-        >
+        <label class="min-w-28 flex items-center gap-2" for="">
+          <IconCalender class="w-4 h-4 stroke-black" />
+          <span> Pickup date: </span>
+        </label>
         <p class="font-semibold">{{ booking.pickup_date }}</p>
       </div>
       <div class="flex gap-2 flex-wrap">
-        <label class="min-w-28 flex items-center gap-2" for=""
-          ><IconTime class="w-4 h-4 fill-none stroke-black" />
-          <span> Pickup time: </span></label
-        >
+        <label class="min-w-28 flex items-center gap-2" for="">
+          <IconTime class="w-4 h-4 fill-none stroke-black" />
+          <span> Pickup time: </span>
+        </label>
         <p class="font-semibold">{{ booking.pickup_time }}</p>
       </div>
       <div v-if="booking.hours" class="flex gap-2 flex-wrap">
-        <label class="min-w-28 flex items-center gap-2" for=""
-          ><IconTime class="w-4 h-4 fill-none stroke-black" />
-          <span> Total hours: </span></label
-        >
+        <label class="min-w-28 flex items-center gap-2" for="">
+          <IconTime class="w-4 h-4 fill-none stroke-black" />
+          <span> Total hours: </span>
+        </label>
         <p class="font-semibold">{{ booking.hours }}</p>
       </div>
     </div>
-    <button
-      @click="bookNow"
-      class="bg-slate-600 rounded-md transition-all hover:bg-slate-800 w-full text-center text-white block mt-10 px-10 py-2.5"
-    >
+    <button @click="bookNow"
+      class="bg-slate-600 rounded-md transition-all hover:bg-slate-800 w-full text-center text-white block mt-10 px-10 py-2.5">
       Book ride now
     </button>
   </div>
--- components/home/Banner/BookingForm.vue
+++ components/home/Banner/BookingForm.vue
@@ -2,86 +2,52 @@
   <div class="">
     <div class="booking-form w-full md:w-[400px]">
       <div class="grid grid-cols-2 w-full bg-[#2A2E31]">
-        <button
-          @click="toggleType('per_km')"
-          :class="[
-            type == 'per_km' ? 'text-[#0693E3] font-semibold ' : 'theme-color',
-          ]"
-          class="relative transition-all hover:text-[#0693E3] flate-rate py-4 border-t-2 border-[#0693E3] uppercase"
-        >
+        <button @click="toggleType('per_km')" :class="[
+          type == 'per_km' ? 'text-[#0693E3] font-semibold ' : 'theme-color',
+        ]"
+          class="relative transition-all hover:text-[#0693E3] flate-rate py-4 border-t-2 border-[#0693E3] uppercase">
           Flat rate
         </button>
-        <button
-          @click="toggleType('hourly')"
-          :class="[
-            type == 'hourly' ? 'text-[#0693E3] font-semibold ' : 'theme-color',
-          ]"
-          class="py-4 transition-all hover:text-[#0693E3] uppercase"
-        >
+        <button @click="toggleType('hourly')" :class="[
+          type == 'hourly' ? 'text-[#0693E3] font-semibold ' : 'theme-color',
+        ]" class="py-4 transition-all hover:text-[#0693E3] uppercase">
           Hourly
         </button>
       </div>
       <form @submit.prevent="getFreeQuote" class="p-4 text-white space-y-3">
         <div>
           <label for="">From</label>
-          <input
-            required
-            v-model="booking.from_location"
-            id="pickupLocation"
+          <input required v-model="booking.from_location" id="pickupLocation"
             class="w-full focus:outline-none focus:border-[#0693E3] py-3 border rounded-md border-2 border-transparent mt-1 bg-[#2A2E31] px-3"
-            type="text"
-            placeholder="Enter full pickup address"
-          />
+            type="text" placeholder="Enter full pickup address" />
         </div>
         <div v-if="type == 'per_km'">
           <label for="">To</label>
-          <input
-            required
-            id="dropLocation"
-            v-model="booking.drop_location"
+          <input required id="dropLocation" v-model="booking.drop_location"
             class="w-full focus:outline-none focus:border-[#0693E3] border rounded-md border-2 border-transparent mt-1 bg-[#2A2E31] py-3 px-2"
-            type="text"
-            placeholder="Enter drop location"
-          />
+            type="text" placeholder="Enter drop location" />
         </div>
         <div v-else>
           <label for="">Hourly</label>
-          <select
-            required
-            v-model="booking.hours"
-            @change="selectHours"
+          <select required v-model="booking.hours" @change="selectHours"
             class="w-full focus:outline-none h-12 focus:border-[#0693E3] border rounded-md border-2 border-transparent mt-1 bg-[#2A2E31] py-3 px-2"
-            name=""
-            id=""
-          >
+            name="" id="">
             <option disabled selected value="">Hourly hire</option>
             <option v-for="(i, index) in 8" :value="i">{{ i }}</option>
           </select>
         </div>
         <div class="relative w-full">
           <label for="">Pickup Date</label>
-          <input
-            v-model="booking.pickup_date"
-            placeholder="dd / mm / yyyy"
-            type="date"
-            required
-            class="w-full mt-1 !text-left pickup-date h-12 border focus:outline-none focus:border-[#0693E3] rounded-md border-2 border-transparent bg-[#2A2E31] !py-3 !px-3"
-          />
-          <IconCalender
-            class="fill-none pointer-events-none top-11 top-10 right-3 absolute stroke-[#0693E3] w-5 h-5"
-          />
+          <input v-model="booking.pickup_date" placeholder="dd / mm / yyyy" type="date" required
+            class="w-full mt-1 !text-left pickup-date h-12 border focus:outline-none focus:border-[#0693E3] rounded-md border-2 border-transparent bg-[#2A2E31] !py-3 !px-3" />
+          <IconCalender class="fill-none pointer-events-none top-11 top-10 right-3 absolute stroke-[#0693E3] w-5 h-5" />
         </div>
         <div class="relative w-full">
           <label for="">Pickup Time</label>
-          <input
-            required
-            v-model="booking.pickup_time"
+          <input required v-model="booking.pickup_time"
             class="w-full mt-1 pickup-date !text-left h-12 border focus:outline-none focus:border-[#0693E3] rounded-md border-2 border-transparent bg-[#2A2E31] !py-3 !px-3"
-            type="time"
-          />
-          <iconTime
-            class="fill-none pointer-events-none top-11 top-10 right-3 absolute stroke-[#0693E3] w-6 h-5"
-          />
+            type="time" />
+          <iconTime class="fill-none pointer-events-none top-11 top-10 right-3 absolute stroke-[#0693E3] w-6 h-5" />
         </div>
         <div class="w-full">
           <button type="submit" @click="toggleModal">
@@ -89,35 +55,20 @@
           </button>
         </div>
         <div>
-          <div
-            v-if="isBookingModal"
-            class="fixed bg-[rgba(0,0,0,0.7)] inset-0"
-          ></div>
-          <div
-            :class="[
-              isBookingModal
-                ? 'top-20 md:top-10 opacity-100 pointer-events-auto'
-                : '-top-full pointer-events-none	opacity-0',
-            ]"
-            class="w-full transition-all duration-500 rounded-md text-black left-0 md:left-[calc(50%-25%)] z-20 md:w-1/2 bg-white fixed px-3 py-8 md:p-8 md:aspect-video"
-          >
-            <GoogleMap
-              api-key="AIzaSyCc0FXkMRujB1PIFO39Z1Bg-Xu1AHGSlwE"
-              :center="mapCenter"
-              :zoom="12"
-              ref="googleMap"
-              class="w-full h-40"
-            >
+          <div v-if="isBookingModal" class="fixed bg-[rgba(0,0,0,0.7)] inset-0"></div>
+          <div :class="[
+            isBookingModal
+              ? 'top-20 md:top-10 opacity-100 pointer-events-auto'
+              : '-top-full pointer-events-none	opacity-0',
+          ]"
+            class="w-full transition-all duration-500 rounded-md text-black left-0 md:left-[calc(50%-25%)] z-20 md:w-1/2 bg-white fixed px-3 py-8 md:p-8 md:aspect-video">
+            <GoogleMap api-key="AIzaSyCc0FXkMRujB1PIFO39Z1Bg-Xu1AHGSlwE" :center="mapCenter" :zoom="12" ref="googleMap"
+              class="w-full h-40">
               <!-- Marker is placed on the map using markerPosition -->
               <Marker :options="{ position: origin || mapCenter }" />
               <!-- <Marker v-if="destination" :options="{ position: destination }" /> -->
             </GoogleMap>
-            <ModalBooking
-              @toggleModal="getFreeQuote"
-              :booking="booking"
-              :origin="origin"
-              :destination="destination"
-            />
+            <ModalBooking @toggleModal="getFreeQuote" :booking="booking" :origin="origin" :destination="destination" />
           </div>
         </div>
       </form>
@@ -129,7 +80,7 @@
 import { ref, reactive, onMounted } from "vue";
 import { GoogleMap, Marker } from "vue3-google-map";
 
-const mapCenter = ref({ lat: 25.2048, lng: 55.2708 });
+const mapCenter = ref({ lat: 34.0012, lng: 71.5249 });
 const googleMap = ref(null);
 const type = ref("per_km");
 const isBookingModal = ref(false);
@@ -210,16 +161,19 @@ onMounted(() => {
 .vdp__time {
   display: none;
 }
+
 .pickup-date::after,
 .pickup-date::before {
   display: none !important;
   opacity: 0 !important;
 }
+
 input,
 select {
   font-size: 14px;
   color: rgb(120, 126, 130);
 }
+
 .flate-rate::after {
   content: "";
   position: absolute;
@@ -231,6 +185,7 @@ select {
   top: 14px;
   opacity: 0.3;
 }
+
 input::placeholder,
 .theme-color {
   color: rgb(120, 126, 130);
